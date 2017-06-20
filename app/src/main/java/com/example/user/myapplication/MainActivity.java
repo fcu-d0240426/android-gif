00c4d2601f2a4dc0f95dcfdb3af5bc46112da5b0
fcu-d0240426 <honzu0927@gmail.com>
2017-06-20 19:19:40 +0800

    final

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.user.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton searchTemp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        searchTemp = (ImageButton)findViewById(R.id.searchTemp);
+        searchTemp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, activity_research_result.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
